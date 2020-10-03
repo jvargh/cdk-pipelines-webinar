4027c8e9c4a92a899f0f2522fa88e9edf37f9d8f
jvargh <joji_varghese@hotmail.com>
2020-10-02 21:45:43 -0400

    changes to lambda

--- pipeline_webinar/lambda/handler.py
+++ pipeline_webinar/lambda/handler.py
@@ -1,5 +1,17 @@
+import json
+
 def handler(event, context):
+  print('request: {}'.format(json.dumps(event)))
   return {
-    'body': 'Oops',
-    'statusCode': '500'
+    'statusCode': 200,
+    'headers': {
+      'Content-Type': 'text/plain'
+    },
+    'body': 'Hello, CDK ! You have hit {}\n'.format(event['path'])
+
   }
+
+  # return {
+  #   'body': 'Oops',
+  #   'statusCode': '500'
+  # }
