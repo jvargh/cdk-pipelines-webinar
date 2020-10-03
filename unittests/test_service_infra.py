677f812a99961c4e5bd2cfe494511bf682059f98
jvargh <joji_varghese@hotmail.com>
2020-10-03 00:19:20 -0400

    added unit tests

--- unittests/test_service_infra.py
+++ unittests/test_service_infra.py
@@ -1,7 +1,6 @@
 from aws_cdk import core
 from pipeline_webinar.pipeline_webinar_stack import PipelineWebinarStack
 
-
 def test_lambda_handler():
     # GIVEN
     app = core.App()
@@ -14,6 +13,6 @@ def test_lambda_handler():
     template = app.synth().get_stack_by_name('Stack').template
     # (3) get lambda fn out of the template
     functions = [resource for resource in template['Resources'].values()
-                 if resource['Type'] == 'AWS::Lambda::Function']
+                    if resource['Type']=='AWS::Lambda::Function']
     assert len(functions) == 1
     assert functions[0]['Properties']['Handler'] == 'handler.handler'
